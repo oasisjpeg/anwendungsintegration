5075c9811b3838a7e0acde6913dbfc2bd546f517
luuc4 <greineckerluca@hotmail.com>
2025-04-29 17:18:24 +0200

    article model added

--- backend/api_backend/api/Domain/Repositories/IArticleRepository.cs
+++ backend/api_backend/api/Domain/Repositories/IArticleRepository.cs
@@ -6,7 +6,7 @@ namespace WebApplication1.Domain.Repositories
     public interface IArticleRepository
     {
         Task<List<ArticleModel>> GetArticleOverviewFromDB();
-        Task<ArticleModel> GetOneCompleteArticleFromDB(int ArticleId);
-        Task SubmitOneQuestionAnswerFromDB(QuizQuestionDto quizQuestionDto);
+        Task<ArticleModel> GetOneCompleteArticleFromDB(int articleId);
+        Task SubmitOneQuestionAnswerFromDB(QuizQuestionDto quizQuestionDto, Guid userId);
     }
 }
--- backend/api_backend/api/Infrastructure/MySqlRepositories/MySqlArticleRepository.cs
+++ backend/api_backend/api/Infrastructure/MySqlRepositories/MySqlArticleRepository.cs
@@ -1,6 +1,63 @@
-﻿namespace WebApplication1.Infrastructure.MySqlRepositories
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Application_Layer.DTO.Quiz;
+using WebApplication1.Domain.Models.Article;
+using WebApplication1.Domain.Models.User;
+using WebApplication1.Domain.Repositories;
+
+namespace WebApplication1.Infrastructure.MySqlRepositories
 {
-    public class MySqlArticleRepository
+    public class MySqlArticleRepository : IArticleRepository
     {
+        private readonly MySqlDbContext _dbContext;
+        public MySqlArticleRepository(MySqlDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<List<ArticleModel>> GetArticleOverviewFromDB()
+        {
+            return await _dbContext.Articles.ToListAsync();
+        }
+
+        public async Task<ArticleModel> GetOneCompleteArticleFromDB(int articleId)
+        {
+            return await _dbContext.Articles
+                .FirstOrDefaultAsync(a => a.ArticleId == articleId);
+        }
+
+        public async Task SubmitOneQuestionAnswerFromDB(QuizQuestionDto quizQuestionDto, Guid userId)
+        {
+            var question = await _dbContext.Question
+                .Include(q => q.Quiz)
+                .FirstOrDefaultAsync(q => q.QuestionId == quizQuestionDto.QuestionId);
+
+            if (question == null)
+            {
+                throw new Exception("Question not found");
+            }
+
+            var userAnswer = new UserAnswerModel
+            {
+                AnswerId = 0,
+                AnsweredAt = DateTime.UtcNow,
+                SelectedAnswer = quizQuestionDto.AnswerSelectionIndex,
+                QuestionId = quizQuestionDto.QuestionId,
+                UserId = int.Parse(userId.ToString()),
+                Question = question
+            };
+            
+            _dbContext.UserAnswer.Add(userAnswer);
+            await _dbContext.SaveChangesAsync();
+        }
+        
+        public async Task<bool> IsLastQuestionInQuiz(int quizId, int questionId)
+        {
+            var quizQuestions = await _dbContext.Question
+                .Where(q => q.QuizId == quizId)
+                .OrderBy(q => q.QuestionId)
+                .ToListAsync();
+            
+            return quizQuestions.Count > 0 && 
+                   quizQuestions.IndexOf(quizQuestions.FirstOrDefault(q => q.QuestionId == questionId)) == 3;
+        }
     }
 }
